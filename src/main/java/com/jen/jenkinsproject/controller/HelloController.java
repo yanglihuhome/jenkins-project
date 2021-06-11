6426d526ea3060ca78324f365a332fcc6e1bed3f
yanglihu <1350437525@qq.com>
2021-06-11 16:40:07 +0800

    提交修改2

---
+++ src/main/java/com/jen/jenkinsproject/controller/HelloController.java
@@ -0,0 +1,18 @@
+package com.jen.jenkinsproject.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author yanglihu
+ * @ClassName HelloController
+ * @Description TODO
+ * @Date 2021/6/11 11:22
+ */
+@Controller
+public class HelloController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello jenkins！！";
+    }
+}
